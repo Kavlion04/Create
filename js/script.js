3140d9028e111994c78acc28444564530cd4372a
aqqullo <xaqqulloabdusamotov@gmail.com>
2024-11-23 02:37:35 +0500

    header

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// set the modal menu element
+const $targetEl = document.getElementById('modalEl');
+
+// options with default values
+const options = {
+  placement: 'bottom-right',
+  backdrop: 'dynamic',
+  backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+  closable: true,
+  onHide: () => {
+      console.log('modal is hidden');
+  },
+  onShow: () => {
+      console.log('modal is shown');
+  },
+  onToggle: () => {
+      console.log('modal has been toggled');
+  }
+};
+
+import { Modal } from 'flowbite';
+
+/*
+* $targetEl: required
+* options: optional
+*/
+const modal = new Modal($targetEl, options);
+// show the modal
+modal.show();
+
+// hide the modal
+modal.hide();
+// toggle the modal
+modal.toggle();
+
+// true if hidden
+modal.isHidden();
+
+// true if visible
+modal.isVisible();
